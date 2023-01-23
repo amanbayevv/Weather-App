94e7e480eab98bc973c2e1c134bc39fada65853f
amanbayevv <diyoramanbayev22@gmail.com>
2023-01-23 21:56:07 +0300

    Weather

--- js/app.js
+++ js/app.js
@@ -3,17 +3,40 @@ const card = document.getElementById('card')
 const details = document.getElementById('details')
 const weatherIcon = document.getElementById('weather-icon')
 const overlay = document.getElementById('overlay')
+const Successbtn = document.getElementById('btn')
 
 
-const updateUI = (weather) => {
+
+
+changeLocation.city.focus()
+
+function loader(state) {
+    if (state) {
+        overlay.classList.remove('d-none')
+    } else {
+        overlay.classList.add('d-none')
+    }
+}
+
+
+
+const updataUI = (weather) => {
     console.log(weather)
     details.innerHTML =`
           <h5 class="mb-3">${weather.name},${weather.sys.country}</h5>
-          <p class="mb-3">CLOUDS</p>
+          <p class="mb-3">${weather.weather[0].main}</p>
           <div class="display-4 mb-3">
-            <span>TEMP</span>
+            <span>${Math.round(weather.main.temp)}</span>
             <span>&deg;C</span>
           </div>`
+
+
+
+          if (card.classList.contains('d-none')) {
+            card.classList.remove('d-none')
+          }
+
+          weatherIcon.src =`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`
 }
 
 
@@ -28,6 +51,14 @@ const getWeather = async (city) => {
 
 
 changeLocation.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const cityName = changeLocation.city.value.trim()
+    changeLocation.reset()
+    getWeather(cityName).then((data) => {
+        updataUI(data)
+    })
+})
+Successbtn.addEventListener('click', (e) => {
     e.preventDefault()
     const cityName = changeLocation.city.value.trim()
     changeLocation.reset()
--- js/forecast.js
+++ js/forecast.js
@@ -5,10 +5,10 @@ const getData = async (city) => {
 const base = 'https://api.openweathermap.org/data/2.5/weather'
 const query = `?q=${city}&units=metric&appid=${KEY}`
 
-
+loader(true)
 const req = await fetch(base + query)
 const data = await req.json()
-
+loader(false)
 return data
 }
 const result = getData('London').then((data) => console.log(data))
